0c2056b2f5db2a4a4963842e079310a534b3ac47
Billy Lee <codingbillylee@gmail.com>
2023-02-25 19:17:54 -0800

    Replace getPrice function with price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,23 @@ function Pizza(size, toppings, price) {
   this.price = price;
 }
 
+Pizza.prototype.fullCost = function() {
+  let price = 10;
+  if (this.size === "large") {
+    price += 2;
+    console.log(price);
+  }
+  price += this.toppings.length;
+  this.price = price;
+  return "This " + this.size + " pizza with" + this.toppings + " costs $" + this.price;
+};
+
 function gatherInfo(event) {
   event.preventDefault();
   let size = document.querySelector("input[name='size']:checked").value;
   let toppings = document.getElementsByName("toppings");
   
-  pushValues(toppings, size, toppingSelection);
+  pushValues(toppings, size);
   getPrice();
 }
 
@@ -17,7 +28,7 @@ function pushValues(toppings, size) {
   let toppingSelection = [];
   for (let i of toppings) {
     if (i.checked) {
-    toppingSelection.push(i.value);
+    toppingSelection.push(" " + i.value);
     }
   }
 
